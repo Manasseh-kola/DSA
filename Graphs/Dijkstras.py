9017bb75a0ee662a72a3f67ec545814a487412be
Manasseh-kola <48407355+Manasseh-kola@users.noreply.github.com>
2021-10-31 20:24:43 +0100

    Create Dijkstras.py

---
+++ Graphs/Dijkstras.py
@@ -0,0 +1,19 @@
+import heapq
+def dijkstrasAlgorithm(start, edges):
+    minDistances = [float("inf") for _ in range(len(edges))]
+    minDistances[start] = 0
+      minHeap = []
+    heapq.heappush(minHeap,(0,start))
+
+    while len(minHeap):
+      currentMinDistance,currentNode = heapq.heappop(minHeap)
+      if currentMinDistance == float("inf"):
+        break
+      for edge in edges[currentNode]:
+        childNode,distance = edge
+        newDistance = currentMinDistance + distance
+        prevDistance = minDistances[childNode]
+        if newDistance < prevDistance:
+          minDistances[childNode] = newDistance
+          heapq.heappush(minHeap,(newDistance,childNode))
+    return list(map(lambda x: -1 if x == float("inf") else x,minDistances))
