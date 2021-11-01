ac22499200ad4f27ed9bdb27d8c2df28a97d3343
Manasseh-kola <48407355+Manasseh-kola@users.noreply.github.com>
2021-11-01 15:59:14 +0100

    Create RadixSort.py

---
+++ Sorting/RadixSort.py
@@ -0,0 +1,47 @@
+"""
+Write a function that takes in an array of nonnegative integers and returns a sorted version
+of that array. Use the Radix Sort algorithm to
+sort the array.
+If you're unfamiliar with Radix Sort, we
+recommend watching the Conceptual
+Overview section of this question's video
+explanation before starting to code.
+
+Sample Input
+array = [8762, 654, 3008, 345, 87, 65,234,12,2]
+
+Sample Output
+array = [2, 12, 65, 87, 234, 345, 654, 3008, 8762]
+"""
+
+def radixSort(array):
+      if len(array) == 0:
+      return array
+
+    maxNumber = max(array)
+    digit = 0
+    while maxNumber // 10 ** digit >0 :
+      array = countingSort(array,digit)
+      digit +=1
+
+    return array
+
+def countingSort(array,digit):
+    sortedArray = [0] * len(array)
+    countArray = [0] * 10
+
+    digitColumn = 10 ** digit
+    for num in array:
+      countIndex = (num//digitColumn) % 10
+      countArray[countIndex] +=1
+
+    for i in range(1,10):
+      countArray[i] += countArray[i-1]
+
+    for i in range(len(array)-1,-1,-1):
+      countIndex = (array[i]//digitColumn)%10
+      countArray[countIndex] -=1
+      sortedIndex = countArray[countIndex]
+      sortedArray[sortedIndex] = array[i]
+
+    return sortedArray
