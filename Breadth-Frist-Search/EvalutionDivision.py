f49ede6b19a8167064618807b84dab3d5cd8c780
Manasseh-kola <48407355+Manasseh-kola@users.noreply.github.com>
2021-11-01 22:02:28 +0100

    Update EvalutionDivision.py

--- Breadth-Frist-Search/EvalutionDivision.py
+++ Breadth-Frist-Search/EvalutionDivision.py
@@ -1,79 +1,60 @@
 """
 LeetCode
-399. Evaluate Division
+130. Surrounded Regions
 
-You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i].
-Each Ai or Bi is a string that represents a single variable. You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer
-for Cj / Dj = ?.
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
 
-Return the answers to all queries. If a single answer cannot be determined, return -1.0.
-Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
-
-Example 1:
-
-Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
-Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
-Explanation: 
-Given: a / b = 2.0, b / c = 3.0
-queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
-return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+Explanation: Surrounded regions should not be on the border, which means that any 'O' on the border of the board are not flipped to 'X'.
+Any 'O' that is not on the border and it is not connected to an 'O' on the border will be flipped to 'X'.
+Two cells are connected if they are adjacent cells connected horizontally or vertically.
 """
 
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        visited = set()
+        flip = []
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if (r,c) in visited:
+                    continue
+                if board[r][c] == "O":
+                    self.bfs(board,r,c,visited,flip)
         
-        adjList = {}
-        index = 0
-        for from_,to in equations:
-            weight = values[index]
-            if from_ not in adjList:
-                adjList[from_] = []
-            if to not in adjList:
-                adjList[to] = []
-           
-            adjList[from_].append((to,weight))
-            adjList[to].append((from_,1/weight))
-               
-            index+=1
-            
-        
-        result = []
-        for start,end in queries:
-            
-            if start not in adjList and end not in adjList:
-                result.append(-1)
-            
-            elif start == end:
-                result.append(1)
-            else:
-                visited = set()
-                productArray = []
-                self.depthFirst(start,start,end,adjList,1,visited,productArray)
-                if len(productArray):
-                    result.append(productArray[0])
-
-                else:
-                    result.append(-1)
+        for currSet in flip:
+            for r,c in currSet:
+                board[r][c] = "X"
                 
+              
+    def bfs(self,graph,sr,sc,visited,flip):
+        R,C = len(graph),len(graph[0])
+        perimeter = 0
+        stack = [(sr,sc)]
+        currentRegion = []
+        marked = 0
+        while stack:
+            r,c = stack.pop()
+            if (r,c) in visited:
+                continue
+            visited.add((r,c))
+            currentRegion.append((r,c))
+            currPerimeter = 4
         
-        return result
-    
-    def depthFirst(self,start,currentNode,end,adjList,product,visited,productArray):
-        
-        if currentNode in visited:
-            return
-        visited.add(currentNode)
-        if currentNode == end:
-            productArray.append(product)
-            return
-       
-        if currentNode not in adjList:
-            return 
-        
-        for branch in adjList[currentNode]:
-            if len(productArray):
-                break
-            branchNode,currentWeight = branch
-            newProduct = product*currentWeight
-            self.depthFirst(start,branchNode,end,adjList,newProduct,visited,productArray)
+            for nr,nc in (r+1,c),(r-1,c),(r,c+1),(r,c-1):
+                if R>nr>=0<=nc<C:
+                    if graph[nr][nc] == "O":
+                        currPerimeter -=1
+                        stack.append((nr,nc))
+                    else:
+                        marked+=1
+            
+            perimeter +=currPerimeter
+            
+        if marked >= perimeter:
+            flip.append(currentRegion)
     
